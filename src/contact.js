b7e953a2ca3380c5c1417f12e0ecc4618a928c9a
Jenna <jenna48071@gmail.com>
2022-01-28 14:47:26 -0600

    changed styling on contact page

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,32 @@
+const placeIcon = (id, src, context) => {
+    const formDiv = document.createElement('div');
+    formDiv.classList.add(id);
+
+    const icon = document.createElement('img');
+    icon.setAttribute('id', id);
+    icon.setAttribute('src', src);
+    formDiv.appendChild(icon);
+
+    const formContext = document.createElement('p');
+    formContext.classList.add('formContext');
+    formContext.innerHTML = context;
+    formDiv.appendChild(formContext);
+
+    return formDiv;
+};
+
 const createForm = () => {
     const contactForm = document.createElement('div');
     contactForm.classList.add('contactForm');
 
-    const phoneNumber = document.createElement('h2');
-    phoneNumber.innerHTML = '123-321-4432'
+    //Phone number
+    const phoneNumber = placeIcon('phone', 'imgs/phone.png', '123-321-4556');
     contactForm.appendChild(phoneNumber);
+
+    //address
+    const address = placeIcon('address', 'imgs/home.png', '389 Amsterdam Ave');
+    contactForm.appendChild(address);
+    //hours
 
     return contactForm;
 }
@@ -13,7 +35,7 @@ const createContactPage = () => {
     // Creating contact section
     const contact = document.createElement('section');
     contact.classList.add('contactSection');
-
+    // Contact container
     const contactContainer = document.createElement('div');
     contactContainer.classList.add('contactContainer');
     contact.appendChild(contactContainer);
@@ -21,14 +43,20 @@ const createContactPage = () => {
     const form = createForm()
     contactContainer.appendChild(form);
 
+    // Creating Map 
     const mapDiv = document.createElement('div');
     mapDiv.classList.add('mapDiv');
     contactContainer.appendChild(mapDiv);
 
-
     const map = document.createElement('p');
     map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.91476950538!2d-74.11976291530915!3d40.6974034425091!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1643357519865!5m2!1sen!2sus" width="450" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>'
     mapDiv.appendChild(map);
+
+    // const attrib = document.createElement('p');
+    // attrib.innerHTML = '<a href="https://www.flaticon.com/free-icons/dessert" title="dessert icons">Dessert icons by Freepik-Flaticon</a><a href="https://www.flaticon.com/free-icons/dessert" title="dessert icons">Dessert icons by Vitaly Gorbachev-Flaticon</a>' +
+    // '<a href="https://www.flaticon.com/free-icons/cake" title="cake icons">Cake icons by Freepik Flaticon</a>'
+    // ;
+    // mapDiv.appendChild(attrib);
 
 
 
